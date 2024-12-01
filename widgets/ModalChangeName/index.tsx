c52af8c4591090d740b3e0187287ce78ea4f35e3
shuverida <bashtarov@outlook.com>
2024-12-02 02:56:56 +0300

    refactor: refactor code

--- widgets/ModalChangeName/index.tsx
+++ widgets/ModalChangeName/index.tsx
@@ -1,54 +1,27 @@
 "use client"
 
-import { useModal } from "@/shared/hooks/useModal"
 import { FC } from "react"
 
 import { ModalFooter } from "@/entities/ModalFooter"
 import { ModalLayout } from "@/shared/layout/ModalLayout"
-import { useUserQuery } from "@/shared/lib/services/user/useUserQuery"
 import { cn } from "@/shared/lib/utils/cn"
-import { Field } from "@/shared/ui/Field"
 import { Typography } from "@/shared/ui/Typography/Typography"
-import { SubmitHandler, useForm } from "react-hook-form"
+import { ChangeName } from "./features/ChangeName"
+import { useChangeName } from "./shared/hooks/useChangeName"
 
 interface IModalCHangeNameProps {}
 
-interface IFormInput {
-	name: string
-	surname: string
-}
-
 export const ModalCHangeName: FC<IModalCHangeNameProps> = () => {
-	const { onClose, onCloseCurrentModal, currentModal } = useModal()
-	const { data } = currentModal
-	const name = data?.changeInfo?.firstName
-	const surname = data?.changeInfo?.surname
-
-	const { userEdit } = useUserQuery()
-	const { mutateAsync: userEditMutate, isPending } = userEdit
 	const {
+		onClose,
+		onSubmit,
 		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
+		errors,
 		watch,
-	} = useForm<IFormInput>({
-		defaultValues: {
-			name: name || "",
-			surname: surname || "",
-		},
-	})
-
-	const onSubmit: SubmitHandler<IFormInput> = async data => {
-		if (data.name.trim() !== name || data.surname.trim() !== surname) {
-			await userEditMutate({
-				name: data.name.trim(),
-				surname: data.surname.trim(),
-			})
-		}
-		reset()
-		onCloseCurrentModal()
-	}
+		isPending,
+		handleSubmit,
+		onCloseCurrentModal,
+	} = useChangeName()
 
 	return (
 		<ModalLayout
@@ -64,48 +37,12 @@ export const ModalCHangeName: FC<IModalCHangeNameProps> = () => {
 							Edit your name
 						</Typography>
 					</div>
-					<div className="flex flex-col gap-2 px-4">
-						<Field
-							variant="primary"
-							label="First name"
-							register={register("name", {
-								required: "Name is required",
-								minLength: {
-									value: 2,
-									message: "Name must be at least 2 characters long",
-								},
-								maxLength: {
-									value: 32,
-									message: "Name must be no more than 32 characters long",
-								},
-							})}
-							minLength={2}
-							maxLength={32}
-							disabled={isPending}
-							errors={errors.name}
-							watch={watch}
-						/>
-						<Field
-							variant="primary"
-							label="Last name"
-							register={register("surname", {
-								required: "Last name is required",
-								minLength: {
-									value: 2,
-									message: "Last name must be at least 2 characters long",
-								},
-								maxLength: {
-									value: 32,
-									message: "Last name must be no more than 32 characters long",
-								},
-							})}
-							minLength={2}
-							maxLength={32}
-							disabled={isPending}
-							errors={errors.surname}
-							watch={watch}
-						/>
-					</div>
+					<ChangeName
+						register={register}
+						errors={errors}
+						watch={watch}
+						isPending={isPending}
+					/>
 				</div>
 				<ModalFooter
 					onClose={onCloseCurrentModal}
